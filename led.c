de3284fce2059b4eb45ebf4c86521272662ca268
Waaflee <agustinwafle@gmail.com>
2018-03-22 23:49:41 -0300

    first iteration (not working)

--- led.c
+++ led.c
@@ -9,51 +9,76 @@ int pinout[] = {DDD0, DDD1, DDD2, DDD3, DDD4, DDD5, DDD6,
 int pinD[] = {PORTD0, PORTD1, PORTD2, PORTD3, PORTD4, PORTD5, PORTD6,
               PORTD7, PORTB0, PORTB1, PORTB2, PORTB3, PORTB4, PORTB5};
 
-int pinA[] = {PORTC0, PORTC1, PORTC2, PORTC3, PORTC4, PORTC5};
+// int pinA[] = {PORTC0, PORTC1, PORTC2, PORTC3, PORTC4, PORTC5};
 
-int *getPort(int pin) {
-  int D[] = {DDRD, PORTD};
-  int B[] = {DDRB, PORTB};
+int getPort(int pin) {
   if (pin <= 7) {
-    return (int *)*D;
+    return PORTD;
   } else {
-    return (int *)*B;
+    return PORTB;
+  }
+}
+
+int getDDPort(int pin) {
+  if (pin <= 7) {
+    return DDRD;
+  } else {
+    return DDRB;
   }
 }
 
 void setPin(int pin, int state) {
-  int *port = getPort(pin);
-  if (state) {
-    port[0] |= _BV(pinout[pin]);
+  int port = getDDPort(pin);
+  if (!state) {
+    port |= _BV(pinout[pin]);
   } else {
-    port[0] &= ~_BV(pinout[pin]);
+    port &= ~_BV(pinout[pin]);
   }
+  // return port;
 }
 
 void pinOn(int pin) {
-  int *puerto = getPort(pin);
-  puerto[1] |= _BV(pinD[pin]);
+  int puerto = getPort(pin);
+  puerto |= _BV(pinD[pin]);
+  // return puerto;
 }
 
 void pinOff(int pin) {
-  int *puerto = getPort(pin);
-  puerto[1] &= ~_BV(pinD[pin]);
+  int puerto = getPort(pin);
+  puerto &= ~_BV(pinD[pin]);
+  // return puerto;
 }
 
 int main(void) {
   /* set pin 5 of PORTB for output*/
-  // DDRD |= _BV(DDD7);
-  setPin(7, 1);
+  DDRD |= _BV(DDD7);
+  DDRD |= _BV(pinout[2]);
+  setPin(8, 0);
+  setPin(12, 1);
+  // int pines[] = {2, 8, 5, 7, 12};
+  // for (int i = 0; i < 5; i++) {
+  //   setPin(pines[i], 0);
+  // }
 
   while (1) {
     /* set pin 5 high to turn led on */
-    // PORTD |= _BV(pinD[7]);
-    pinOn(7);
+    PORTD |= _BV(pinD[7]);
+    PORTD |= _BV(pinD[8]);
+    pinOn(2);
+    pinOn(12);
+    // for (int i = 0; i < 5; i++) {
+    //   pinOn(pines[i]);
+    // }
     _delay_ms(BLINK_DELAY_MS);
 
     /* set pin 5 low to turn led off */
-    // PORTD &= ~_BV(pinD[7]);
-    pinOff(7);
+    PORTD &= ~_BV(pinD[7]);
+    PORTD &= ~_BV(pinD[8]);
+    pinOff(2);
+    pinOff(12);
+    // for (int i = 0; i < 5; i++) {
+    //   pinOff(pines[i]);
+    // }
     _delay_ms(BLINK_DELAY_MS);
   }
 }
