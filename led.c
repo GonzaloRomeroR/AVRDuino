d71753f86ecf30805a13e70c2b3e736bf00bc355
Waaflee <agustinwafle@gmail.com>
2018-03-26 01:44:44 -0300

    added up to 4 steppers setup for testing

--- led.c
+++ led.c
@@ -4,16 +4,19 @@
 
 #define FALSE 0
 #define TRUE 1
-#define NUM_STEPPERS 2
+#define NUM_STEPPERS 4
 
-volatile int count[2];
+volatile int count[NUM_STEPPERS];
 volatile int delay;
 
 STEPPER *PAParray[NUM_STEPPERS];
 
 ISR(TIMER0_OVF_vect, ISR_NOBLOCK) {
-  count[0]++;
-  count[1]++;
+  for (uint8_t i = 0; i < NUM_STEPPERS; i++) {
+    count[i]++;
+  }
+  // count[0]++;
+  // count[1]++;
   for (uint8_t i = 0; i < NUM_STEPPERS; i++) {
     if (PAParray[i]->enabled) {
       delay = (60 / PAParray[i]->motor->RPM) * 61;
@@ -37,10 +40,14 @@ ISR(TIMER0_OVF_vect, ISR_NOBLOCK) {
 
 int main(void) {
   DriveArray STPArray1 = {2, 3, 4, 0, 0, 0, 1.8, 30};
-  DriveArray STPArray2 = {5, 6, 12, 0, 0, 0, 1.8, 60};
+  DriveArray STPArray2 = {5, 6, 7, 0, 0, 0, 1.8, 60};
+  DriveArray STPArray3 = {8, 9, 10, 0, 0, 0, 1.8, 60};
+  DriveArray STPArray4 = {11, 12, 13, 0, 0, 0, 1.8, 60};
 
   pololu STP1 = newPololuFA(STPArray1);
   pololu STP2 = newPololuFA(STPArray2);
+  pololu STP3 = newPololuFA(STPArray3);
+  pololu STP4 = newPololuFA(STPArray4);
 
   STEPPER PAP1;
   PAP1.motor = &STP1;
@@ -50,15 +57,27 @@ int main(void) {
   PAP2.motor = &STP2;
   PAP2.enabled = 0;
 
+  STEPPER PAP3;
+  PAP2.motor = &STP3;
+  PAP2.enabled = 0;
+
+  STEPPER PAP4;
+  PAP2.motor = &STP4;
+  PAP2.enabled = 0;
+
   PAParray[0] = &PAP1;
   PAParray[1] = &PAP2;
+  PAParray[2] = &PAP3;
+  PAParray[3] = &PAP3;
 
   for (uint8_t i = 0; i < NUM_STEPPERS; i++) {
     count[i] = 0;
   }
 
-  rotateNSteps(5, &PAP1, FORWARD);
+  rotateNSteps(5, &PAP1, BACKWARD);
   rotateNSteps(10, &PAP2, FORWARD);
+  rotateNSteps(20, &PAP3, BACKWARD);
+  rotateNSteps(15, &PAP4, FORWARD);
 
   setTimer0(T0_PRESCALER_256);
 
