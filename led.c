a7866d67292b3925c8c96f49f1b21023161d04db
Waaflee <agustinwafle@gmail.com>
2018-03-26 02:09:23 -0300

    fixed initialitation issue

--- led.c
+++ led.c
@@ -38,9 +38,9 @@ ISR(TIMER0_OVF_vect, ISR_NOBLOCK) {
 
 int main(void) {
   DriveArray STPArray1 = {2, 3, 4, 0, 0, 0, 1.8, 30};
-  DriveArray STPArray2 = {5, 6, 7, 0, 0, 0, 1.8, 60};
+  DriveArray STPArray2 = {5, 6, 7, 0, 0, 0, 1.8, 30};
   DriveArray STPArray3 = {8, 9, 10, 0, 0, 0, 1.8, 60};
-  DriveArray STPArray4 = {11, 12, 13, 0, 0, 0, 1.8, 60};
+  DriveArray STPArray4 = {11, 12, 13, 0, 0, 0, 1.8, 30};
 
   pololu STP1 = newPololuFA(STPArray1);
   pololu STP2 = newPololuFA(STPArray2);
@@ -56,12 +56,12 @@ int main(void) {
   PAP2.enabled = 0;
 
   STEPPER PAP3;
-  PAP2.motor = &STP3;
-  PAP2.enabled = 0;
+  PAP3.motor = &STP3;
+  PAP3.enabled = 0;
 
   STEPPER PAP4;
-  PAP2.motor = &STP4;
-  PAP2.enabled = 0;
+  PAP4.motor = &STP4;
+  PAP4.enabled = 0;
 
   PAParray[0] = &PAP1;
   PAParray[1] = &PAP2;
@@ -72,10 +72,10 @@ int main(void) {
     count[i] = 0;
   }
 
-  rotateNSteps(5, &PAP1, BACKWARD);
+  rotateNSteps(5, &PAP1, FORWARD);
   rotateNSteps(10, &PAP2, FORWARD);
-  rotateNSteps(20, &PAP3, BACKWARD);
-  rotateNSteps(15, &PAP4, FORWARD);
+  rotateNSteps(20, &PAP3, FORWARD);
+  rotateNSteps(60, &PAP4, FORWARD);
 
   setTimer0(T0_PRESCALER_256);
 
@@ -83,5 +83,6 @@ int main(void) {
   // setTimer0PS(T0_PRESCALER_64);
 
   while (1) {
+    // busy loop
   }
 }
